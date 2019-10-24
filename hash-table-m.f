1193a9bb1e456d69670c6d49c995eb2c8c8c8783
DouglasBHoffman <56588547+DouglasBHoffman@users.noreply.github.com>
2019-10-24 06:17:38 -0400

    Add files via upload

--- hash-table-m.f
+++ hash-table-m.f
@@ -1,14 +1,11 @@
 \ This software is free for use and modification by anyone for any purpose
 \ with no restrictions or source identification of any kind.
-\ Oct 16 2019 Douglas B. Hoffman
+\ Oct 24 2019 Douglas B. Hoffman
 \ dhoffman888@gmail.com
 
 
 
 [undefined] hash-table [if] .( requires class hash-table to be loaded) abort [then]
-
-
-
 
 
 \ Since the same join attribute, Name, occurs more than once
@@ -23,8 +20,10 @@
 
 :class hash-table-m <super hash-table
 
-\ called within :insert method in superclass 
- :m :search ( node idx hash -- idx hash false )
+\ Called within :insert method in superclass. 
+\ Key is already present so must chain in val.
+ :m :search ( node idx hash val -- idx hash false )
+    drop
     rot drop false ;m 
 
  :m :next ( -- val true | false )
@@ -40,40 +39,4 @@
 
 
 ;class
-
- 
- 
- 
-0 [if]
-
-hash-table-m r  
-
-s" Whales "   >string s" Jonah" r :insert
-s" Spiders "  >string s" Jonah" r :insert
-s" Ghosts "   >string s" Alan"  r :insert
-s" Buffy "    >string s" Glory" r :insert 
-s" Zombies "  >string s" Alan"  r :insert
-s" Vampires " >string s" Jonah" r :insert
- 
-R is redefined ok
- 
-
-
-s" Jonah" r :@ . .  -1 12630160 ok
-
-12630160 :. \ Whales  ok Whales  ok
-
-r :next . \  -1 ok \ 1  -1 ok \ 1 
-
-:. \ Spiders  ok Spiders  ok
-
-r :next . \ -1 ok \ 1  -1 ok \ 1 
-
-:. \ Vampires  ok Vampires  ok
-
-r :next . \ 0 ok 0 ok
-
-
-
-[then]
 
